e8ee8ce274729cf4bf94146dbc587e22fc1c6078
Mivuka <muokakeinato@gmail.com>
2022-09-08 06:28:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter directory");
+        string filesDirectory = Console.ReadLine();
+
+        var txtFilesPaths = Directory.EnumerateFiles(filesDirectory, "*.txt");
+
+        var sb = new StringBuilder();
+
+        foreach (var txtFilePath in txtFilesPaths)
+        {
+            var text = File.ReadAllTextAsync(txtFilePath, CancellationToken.None).Result;
+            sb.Append(text + "\n");
+        }
+
+        File.WriteAllTextAsync($"{filesDirectory}//Summary (total files counted {txtFilesPaths.Count()}).txt", 
+            sb.ToString(), 
+            Encoding.Unicode,
+            CancellationToken.None);
+    }
+}
